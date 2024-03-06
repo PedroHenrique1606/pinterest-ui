801d4e5bdc0f4dcea8e4e2698dac7fc7d3dd91b0
PedroHenrique1606 <pedromelosilva11911@gmail.com>
2024-03-06 12:44:35 -0300

    menu finalized

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -3,13 +3,33 @@ import BottomSheet from "@gorhom/bottom-sheet";
 import { forwardRef } from "react";
 import { MenuProps } from "./menu";
 import { FontAwesome } from "@expo/vector-icons";
+import { styles } from "./styles";
+import { theme } from "@/theme";
+import { MenuButton } from "../MenuButton";
 
 export const Menu = forwardRef<BottomSheet, MenuProps> (({ onClose }, ref) => {
     return(
-        <BottomSheet ref={ref} index={0} snapPoints={[0.01, 230]}>
-            <View>
-                <FontAwesome name="close" size={24} onPress={onClose}/>
-                <Text>Comece a criar agora</Text>
+        <BottomSheet 
+        ref={ref} 
+        index={0} 
+        snapPoints={[0.01, 230]} 
+        backgroundStyle={styles.container}
+        handleComponent={() => null}>
+            <View style={styles.content}>
+                <View style={styles.header}>
+                    <FontAwesome 
+                    color={theme.colors.white} 
+                    name="close" size={24} 
+                    onPress={onClose}/>
+                    <Text style={styles.title}>
+                        Comece a criar agora
+                    </Text>  
+                </View>
+                <View style={styles.options}>
+                    <MenuButton title="Pin" icon="home"/>
+                    <MenuButton title="Colagem" icon="paste"/>
+                    <MenuButton title="Pasta" icon="folder"/>
+                </View>
             </View>
         </BottomSheet>
     )
---
+++ src/components/MenuButton/index.tsx
@@ -0,0 +1,16 @@
+import { Pressable, Text } from "react-native";
+import { MenuButtonProps } from "./menubutton";
+import { styles } from "./styles";
+import { FontAwesome } from  '@expo/vector-icons'
+import { theme } from "@/theme";
+
+export function MenuButton({title, icon}: MenuButtonProps){
+    return <Pressable style={styles.container}>
+        <FontAwesome 
+        name={icon}  
+        size={32} 
+        style={styles.icon} 
+        color={theme.colors.white}/>
+        <Text style={styles.text}>{title}</Text>
+    </Pressable>
+}
